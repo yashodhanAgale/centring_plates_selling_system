ddd27f01952240108d2d53aec3dba449ad0fab9e
yashodhanAgale <yashodhanagale3@gmail.com>
2025-01-07 10:32:49 +0530

    normal

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use("/request", requestRoutes);
 app.use("/otp", verificationRoutes);
 app.use("/admin", adminRoutes);
 
-const PORT = process.env.DB_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
