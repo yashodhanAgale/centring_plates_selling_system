d4d3f0ee393a1eb362a40af4199cb85892345f66
yashodhanAgale <yashodhanagale3@gmail.com>
2025-01-07 10:13:59 +0530

    normal changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: "",
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
