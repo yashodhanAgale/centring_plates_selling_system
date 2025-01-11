91e3f3e1fd683f1fc656596affa14ad13345fbce
yashodhanAgale <yashodhanagale3@gmail.com>
2025-01-11 07:58:43 +0530

    updating backend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ const app = express();
 require("dotenv").config();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors(
+    cors({
+      origin: "https://centring-plates-selling-fm3w.vercel.app",
+    })
+  )
+);
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded data
