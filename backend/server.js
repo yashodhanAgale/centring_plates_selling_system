0ca60f6bcb11608472ee8bf115dde1139164d0b1
yashodhanAgale <yashodhanagale3@gmail.com>
2025-01-11 15:18:05 +0530

    .env change frontend

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,12 @@ const app = express();
 require("dotenv").config();
 
 // Middleware
-app.use(
-  cors({
-    origin: "https://centring-plates-selling-fm3w.vercel.app",
-  })
-);
-
+// app.use(
+//   cors({
+//     origin: "https://centring-plates-selling-fm3w.vercel.app",
+//   })
+// );
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded data
